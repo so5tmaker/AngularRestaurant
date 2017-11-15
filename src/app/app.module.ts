d229fad85369aacb2437738b1cb1b3a5764fc556
Viktor Sivulskiy <svicvg@gmail.com>
2017-11-15 15:04:13 +0600

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -38,7 +39,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,
+  PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
