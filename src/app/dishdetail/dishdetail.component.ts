3ebe0f6d488c27cc7b5a7ce110320fcfacc1dd4e
Viktor Sivulskiy <svicvg@gmail.com>
2017-11-25 16:38:27 +0600

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -25,6 +25,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   comment: Comment;
   date = new Date();
+  errMess: string;
 
   formErrors = {
     'comment': '',
@@ -54,7 +55,8 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+      errmess => this.errMess = <any>errmess);
   }
 
   createForm() {
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,6 +11,8 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+//import 'rxjs/observable/throw';
 
 @Injectable()
 export class DishService {
@@ -20,22 +22,26 @@ export class DishService {
 
     getDishes(): Observable<Dish[]> {
       return this.http.get(baseURL + 'dishes')
-                      .map(res => { return this.processHTTPMsgService.extractData(res); });
+                      .map(res => { return this.processHTTPMsgService.extractData(res); })
+                      .catch(error => { return this.processHTTPMsgService.handleError(error); });
     }
   
     getDish(id: number): Observable<Dish> {
       return  this.http.get(baseURL + 'dishes/'+ id)
-                      .map(res => { return this.processHTTPMsgService.extractData(res); });
+                      .map(res => { return this.processHTTPMsgService.extractData(res); })
+                      .catch(error => { return this.processHTTPMsgService.handleError(error); });
     }
   
     getFeaturedDish(): Observable<Dish> {
       return this.http.get(baseURL + 'dishes?featured=true')
-                      .map(res => { return this.processHTTPMsgService.extractData(res)[0]; });
+                      .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+                      .catch(error => { return this.processHTTPMsgService.handleError(error); });
     }
   
     getDishIds(): Observable<number[]> {
       return this.getDishes()
-        .map(dishes => { return dishes.map(dish => dish.id) });
+        .map(dishes => { return dishes.map(dish => dish.id) })
+        .catch(error => { return Observable.of(error); } );
     }
 
 }
