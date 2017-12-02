b88450217be41a591c5519ef9fed0f7ba2b2a1bc
Viktor Sivulskiy <svicvg@gmail.com>
2017-12-02 17:13:37 +0600

    Component Test

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -3,6 +3,7 @@ import { Directive, ElementRef, Renderer2, HostListener  } from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
+
 export class HighlightDirective {
 
   constructor(private el: ElementRef,
